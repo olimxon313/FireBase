7d8b89525c7cd0c145b34fbe3973c1ddc733b56e
Olimxon <Olimxon401@gmail.com>
2024-12-08 20:59:47 +0500

    finished

--- app/auth/Login/index.jsx
+++ app/auth/Login/index.jsx
@@ -1,17 +1,30 @@
+'use client';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { useState } from 'react';
 import './login.scss';
 
+
 const Login = () => {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Login submitted');
+    try {
+      await signInWithEmailAndPassword(auth, email ,password)
+      console.log("Login successful");
+      
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
     <div className="login-container">
       <h2 className="login-title">Login</h2>
       <form onSubmit={handleSubmit} className="login-form">
-        <input type="email" placeholder="Email" required className="login-input" />
-        <input type="password" placeholder="Password" required className="login-input" />
+        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required className="login-input" />
+        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required className="login-input" />
         <button type="submit" className="login-button">Login</button>
       </form>
     </div>
--- app/auth/Register/index.jsx
+++ app/auth/Register/index.jsx
@@ -1,18 +1,53 @@
+'use client';
 import './register.scss';
+import React, { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth,db } from '../../firebase';
+import { setDoc,doc } from 'firebase/firestore';
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [fname,setFname]=useState('');
+  const handleRegister = async (e) => {
     e.preventDefault();
-    console.log('Register submitted');
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      console.log(user);
+      if(user){
+        await setDoc(doc(db,"Users",user.uid),{
+          email:user.email,
+          firstName: fname
+        }); 
+      }
+      console.log('Registration successful');
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
     <div className="register-container">
       <h2 className="register-title">Register</h2>
-      <form onSubmit={handleSubmit} className="register-form">
-        <input type="text" placeholder="Username" required className="register-input" />
-        <input type="email" placeholder="Email" required className="register-input" />
-        <input type="password" placeholder="Password" required className="register-input" />
+      <form onSubmit={handleRegister} className="register-form">
+        <input type="text" onChange={(e) => setFname(e.target.value)} placeholder="First Name" required className="register-input" />
+        <input
+          type="email"
+          placeholder="Email"
+          required
+          className="register-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className="register-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit" className="register-button">Register</button>
       </form>
     </div>
